aa09571a3022165e96974cb5fc4e7a38a9a226ba
Tom Nuse <thomasnuse1273@gmail.com>
2024-05-13 16:34:00 -0400

    Week 09 Coding Project

--- mysql-java/src/main/java/projects/ProjectsApp.java
+++ mysql-java/src/main/java/projects/ProjectsApp.java
@@ -20,6 +20,9 @@ public class ProjectsApp {
 
 	// Scanner for user input
 	private Scanner scanner = new Scanner(System.in);
+	
+	// variable instance of type ProjectService named projectService calling on ProjectService class in
+	// projects.service package
 	private ProjectService projectService = new ProjectService();
 	
 	
@@ -35,7 +38,7 @@ public class ProjectsApp {
 	}
 
 	// processUserSelections Method using a try/catch block for validation of user input and returning 
-	// a message to try again if an invalid selection is mad.  Also setting up a Switch inside of the try catch block
+	// a message to try again if an invalid selection is made.  Also setting up a Switch inside of the try catch block
 	// to call methods based on the user selection.
 	
 	
@@ -67,6 +70,76 @@ public class ProjectsApp {
 		}
 	} // END of processUserSelections
 
+	// printOperations method to print out selection menu
+	private void printOperations() {
+		System.out.println("\nThere are the available selections.  (Press enter key to quit):");
+		
+		operations.forEach(line -> System.out.println(" " + line));
+	} // END of printOperations
+	
+	
+	// getUserSelection method to get the user selection from the menu.
+	private int getUserSelection() {
+		printOperations();
+		
+		Integer input = getIntInput("Enter menu selection: ");
+		
+		return Objects.isNull(input) ? -1 : input;
+		
+	} // END of getUserSelection Method
+	
+
+	// getStringInput method for the notes and project_name columns in the project table.
+	
+	private String getStringInput(String prompt) {
+		System.out.print(prompt + ": ");
+		String input = scanner.nextLine();
+		return input.isBlank() ? null : input.trim();
+	
+	} // END of get StringInput method
+
+	
+	// getInInput method to attempt to convert scanner String input to an Integer for the difficulty column in the project table.
+	private Integer getIntInput(String prompt) {
+		String input = getStringInput(prompt);
+		
+		if (Objects.isNull(input)) {
+			return null;
+		}
+		
+		try {
+			return Integer.valueOf(input);
+			
+		} catch (NumberFormatException e) {
+			throw new DbException(input + " is not a valid selection.");
+			
+		}
+		
+	} // END of getInInput
+	
+	
+	// getBigDecimal method to convert the BigDecimal value of prompt to a number with 2 digits to the right of the decimal 
+	// place, conforming it to the Decimal requirements in the estimated_hours and actual_hours columns in the project schema.
+	
+	private BigDecimal getDecimalInput(String prompt) {
+	String input = getStringInput(prompt);
+		
+		if (Objects.isNull(input)) {
+			return null;
+		}
+		
+		try {
+			return new BigDecimal(input).setScale(2);
+			
+		} catch (NumberFormatException e) {
+			throw new DbException(input + " is not a valid decimal number.");
+			
+		}
+		
+				
+	} // End of getBigDecimal method
+	
+
 	// createProject method to create a project using a project name, estimated hours, actual hours, difficulty, and notes
 	// for the project.entity/project class.
 	private void createProject() {
@@ -91,83 +164,15 @@ public class ProjectsApp {
 		
 	}  // END of createProject Method
 	
-	// getBigDecimal method to convert the BigDecimal value of prompt to a number with 2 digits to the right of the decimal 
-	// place, conforming it to the Decimal requirements in the estimated_hours and actual_hours columns in the project schema.
-	
-	private BigDecimal getDecimalInput(String prompt) {
-	String input = getStringInput(prompt);
-		
-		if (Objects.isNull(input)) {
-			return null;
-		}
-		
-		try {
-			return new BigDecimal(input).setScale(2);
-			
-		} catch (NumberFormatException e) {
-			throw new DbException(input + " is not a valid decimal number.");
-			
-		}
-		
-				
-	} // End of getBigDecimal method
-	
 
 	private boolean exitMenu() {
 		System.out.println("Exiting the menu.");
 		return true;
 	}
 
-	// getInInput method to attempt to convert scanner String input to an Integer for the difficulty column in the project table.
-	private Integer getIntInput(String prompt) {
-		String input = getStringInput(prompt);
-		
-		if (Objects.isNull(input)) {
-			return null;
-		}
-		
-		try {
-			return Integer.valueOf(input);
-			
-		} catch (NumberFormatException e) {
-			throw new DbException(input + " is not a valid selection.");
-			
-		}
-		
-	} // END of getInInput
 	
 	
 
-	// getUserSelection method to get the user selection from the menu.
-	private int getUserSelection() {
-		printOperations();
-		
-		Integer input = getIntInput("Enter menu selection: ");
-		
-		return Objects.isNull(input) ? -1 : input;
-		
-	} // END of getUserSelection Method
-	
-
-	
-	// getStringInput method for the notes and project_name columns in the project table.
-	
-	private String getStringInput(String prompt) {
-		System.out.print(prompt + ": ");
-		String input = scanner.nextLine();
-		return input.isBlank() ? null : input.trim();
-	
-	} // END of get StringInput method
-	
-
-	// printOperations method to print out selection menu
-	private void printOperations() {
-		System.out.println("\nThere are the available selections.  (Press enter key to quit):");
-		
-		operations.forEach(line -> System.out.println(" " + line));
-	} // END of printOperations
-	
-	
 
 }
 
